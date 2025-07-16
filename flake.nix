c3830d4246beaeae07f8eb646d43103ddf12db3d
Simon Walker <simon@simonwalker.tech>
2025-07-16 06:53:16 -0600

    sveltekitwind init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            plantuml
+            bun
+          ];
+        };
+      }
+    );
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<article class="prose">
+	<h1>Heading 1</h1>
+	<p>This is a paragraph of text with some <strong>bold</strong> and <em>italic</em> content.</p>
+	<ul>
+		<li>List item 1</li>
+		<li>List item 2</li>
+	</ul>
+	<pre><code>console.log('Hello, Tailwind 4!');</code></pre>
+</article>
